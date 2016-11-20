16c131cd4e145169816b44f0ac06ab2fa121b6f2
Andrew McWilliams <andy@jahya.net>
2016-11-20 02:56:59 -0500

    Switches to same main file as ofxLayerMask, tested on ARM / RPi / Arch Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
-int main()
-{
-    ofGLFWWindowSettings settings;
-    settings.stencilBits = 8;
-
+//========================================================================
+int main( ){
+#ifdef TARGET_OPENGLES
+    ofGLESWindowSettings settings;
+    settings.glesVersion = 2;
     auto window = ofCreateWindow(settings);
     auto app = make_shared<ofApp>();
     ofRunApp(window, app);
-
     return ofRunMainLoop();
+#else
+    ofSetupOpenGL(1024,768,OF_WINDOW);
+    ofRunApp(new ofApp());
+#endif
 }
